a77ea63f319e142c0c7812142f27fb6315229f46
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-10-06 00:37:56 +0900

    Update PostRequestDto.java
    .

--- dto/PostRequestDto.java
+++ dto/PostRequestDto.java
@@ -1,13 +1,9 @@
 package com.sparta.week03.dto;
 
-//
-//에러해결
-
 import com.sparta.week03.domain.Post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-//Post.java에 뜨는 에러를 해결하려면 PostRequestDto.java를 만들어야 한다.
 @Getter
 @AllArgsConstructor
 public class PostRequestDto {
@@ -17,7 +13,6 @@ public class PostRequestDto {
     private String author;
     private String password;
 
-    //dto를 entity로 바꿔준다.
     public Post toEntity(){
         return new Post(title, author, content, password);
     }
