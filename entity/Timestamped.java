08d6c72dd486366b68d056be0870179f1d26c517
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-10-06 00:38:40 +0900

    Update Timestamped.java
    .

--- entity/Timestamped.java
+++ entity/Timestamped.java
@@ -1,7 +1,5 @@
 package com.sparta.week03.entity;
 
-//시간 기록
-
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,8 +10,8 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
-@EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
+@MappedSuperclass 
+@EntityListeners(AuditingEntityListener.class) 
 public class Timestamped {
 
     @CreatedDate
