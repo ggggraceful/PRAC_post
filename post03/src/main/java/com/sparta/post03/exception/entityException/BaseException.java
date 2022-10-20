f7a61f6758657bd43eef503eb205d7726393c5ef
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-10-20 17:40:19 +0900

    Add files via upload
    .

---
+++ post03/src/main/java/com/sparta/post03/exception/entityException/BaseException.java
@@ -0,0 +1,4 @@
+package com.sparta.post03.exception.entityException;
+
+public abstract class BaseException extends RuntimeException{
+}
---
+++ post03/src/main/java/com/sparta/post03/exception/entityException/postException/TitleNotFoundException.java
@@ -0,0 +1,6 @@
+package com.sparta.post03.exception.entityException.postException;
+
+import com.sparta.post03.exception.entityException.BaseException;
+
+public class TitleNotFoundException extends BaseException {
+}
