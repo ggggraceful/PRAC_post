b25a376ff7c75c05832873cbb16890943527e2ef
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2022-12-14 23:10:04 +0900

    Delete code-colosseum-backend directory

--- code-colosseum-backend/src/main/java/com/sparta/codecolosseumbackend/repository/RefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.codecolosseumbackend.repository;
-
-
-import com.sparta.codecolosseumbackend.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-}
